a589af91aa7461ce795f7e17db41cf69cf2ee960
OleksandrSam <softinverse@gmail.com>
2022-04-01 09:03:40 +0300

    book selection by id, name (asc,desc), count was done

--- book/views.py
+++ book/views.py
@@ -1,8 +1,13 @@
+from audioop import reverse
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from book.models import Book
 
 
-def all_book(request):
+def all_book(request, sort='id'):
     context = Book.get_all()
+    if sort == 'id': context.sort(key=lambda book: book.id)
+    if sort == 'count': context.sort(key=lambda book: book.count)
+    if sort == 'name_asc': context.sort(key=lambda book: book.name, reverse=False)
+    if sort == 'name_desc': context.sort(key=lambda book: book.name, reverse=True)
     return render(request, 'book/all_book.html', {'context': context})
