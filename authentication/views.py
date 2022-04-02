62a8cecbecf0504bf63d50de98b4bbff49aa8a1d
Nazar Ravlyk <ravlyk.n@gmail.com>
2022-04-02 22:00:34 +0300

    master version

--- authentication/views.py
+++ authentication/views.py
@@ -8,45 +8,48 @@ import datetime
 import pytz
 
 def add_info(request):
+    try:
+        user = CustomUser(id=111, email='email@mail.com', password='1234', first_name='Іван',
+                                middle_name='',
+                                last_name='Франко')
+        user.save()
+        user_free = CustomUser(id=222, email='2email@mail.com', password='1234', first_name='Леся',
+                                    middle_name='',
+                                    last_name='Українка')
+        user_free.save()
 
-    user = CustomUser(id=111, email='email@mail.com', password='1234', first_name='fname',
-                            middle_name='mname',
-                            last_name='lname')
-    user.save()
-    user_free = CustomUser(id=222, email='2email@mail.com', password='1234', first_name='2fname',
-                                middle_name='2mname',
-                                last_name='2lname')
-    user_free.save()
+        author1 = Author(id=101, name="Тарас", surname="Шевченко", patronymic="Григорович")
+        author1.save()
 
-    author1 = Author(id=101, name="author1", surname="s1", patronymic="p1")
-    author1.save()
+        author2 = Author(id=102, name="Іван", surname="Котляревський", patronymic="Петрович")
+        author2.save()
 
-    author2 = Author(id=102, name="author2", surname="s2", patronymic="p2")
-    author2.save()
+        book1 = Book(id=1, name="Кобзар", description="Опис книжки Кобзар", count=5)
+        book1.save()
+        book1.authors.add(author1)
+        book1.save()
 
-    book1 = Book(id=101, name="book1", description="description1", count=1)
-    book1.save()
-    book1.authors.add(author1)
-    book1.save()
+        book2 = Book(id=2, name="Енеїда", description="Опис книжки Енеїда")
+        book2.save()
+        book2.authors.add(author2)
+        book2.save()
 
-    book2 = Book(id=102, name="book2", description="description2")
-    book2.save()
-    book2.authors.add(author2)
-    book2.save()
+        book3 = Book(id=103, name="Автобіографії", description="Автобіографії", count=11)
+        book3.save()
+        book3.authors.add(author1)
+        book3.authors.add(author2)
+        book3.save()
 
-    book3 = Book(id=103, name="book3", description="description3")
-    book3.save()
-    book3.authors.add(author1)
-    book3.authors.add(author2)
-    book3.save()
+        TEST_DATE = datetime.datetime(2023, 4, 10, 12, 00, tzinfo=pytz.utc)
+        TEST_DATE_END = TEST_DATE + datetime.timedelta(days=15)
 
-    TEST_DATE = datetime.datetime(2023, 4, 10, 12, 00, tzinfo=pytz.utc)
-    TEST_DATE_END = TEST_DATE + datetime.timedelta(days=15)
-
-    order1 = Order(id=101, user=user, book=book1, plated_end_at=TEST_DATE)
-    order1.save()
-    order2 = Order(id=102, user=user, book=book2, plated_end_at=TEST_DATE)
-    order2.save()
-    order3 = Order(id=103, user=user, book=book3, end_at=TEST_DATE_END, plated_end_at=TEST_DATE)
-    order3.save()  
-    return render(request, 'library/index.html')
+        order1 = Order(id=51, user=user, book=book1, plated_end_at=TEST_DATE)
+        order1.save()
+        order2 = Order(id=52, user=user, book=book2, plated_end_at=TEST_DATE)
+        order2.save()
+        order3 = Order(id=53, user=user, book=book3, end_at=TEST_DATE_END, plated_end_at=TEST_DATE)
+        order3.save()  
+    except: 
+        pass
+    finally:    
+        return render(request, 'library/index.html')
