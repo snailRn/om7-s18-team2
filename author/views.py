3866cdf049ab8e6f8250e4cdaf8c3dce2b670549
YaroslavBorysko <yaroslavborysko@gmail.com>
2022-04-07 00:02:59 +0300

    implemented author books method

--- author/views.py
+++ author/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from authentication.models import CustomUser
 from author.models import Author
+from book.models import Book
 
 from authentication.forms import CustomUserForm
 from author.forms import AuthorForm
@@ -44,3 +45,12 @@ def delete_author(request, id=0):
 
 
 
+def author_books(request, id=0):
+    authors_book = []
+
+    for book in Book.objects.all():
+        for author in book.authors.all():
+            if author.id == id: authors_book.append(book)
+    title = 'books'
+    content = f'Selected all book by author id = {id}'
+    return render(request, 'book/all_book.html', {'title': title, 'content': content, 'context': authors_book})
